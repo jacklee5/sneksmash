ecd5995c2642c59f76fcff5998c84ffe1e2934a2
Jack Lee <jack200336@gmail.com>
2018-05-30 11:37:59 -0400

    more stuff

---
+++ server.js
@@ -0,0 +1,25 @@
+const p2 = require("p2");
+// Dependencies
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var socketIO = require('socket.io');
+var app = express();
+var server = http.Server(app);
+var io = socketIO(server);
+app.set('port', 5000);
+app.use('/static', express.static(__dirname + '/static'));
+// Routing
+app.get('/', function(request, response) {
+  response.sendFile(path.join(__dirname, 'index.html'));
+});
+// Starts the server.
+server.listen(5000, function() {
+  console.log('Starting server on port 5000');
+});
+
+io.on('connection', function(socket) {
+    socket.on("new player", () => {
+        console.log("player joined :)))");
+    })
+}
