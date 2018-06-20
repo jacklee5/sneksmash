cb8f99cb38dd2db22112e52b5ff6c4ef68f9632b
Jack Lee <jack200336@gmail.com>
2018-06-20 19:54:21 -0400

    support rho

--- server.js
+++ server.js
@@ -13,8 +13,10 @@ app.get('/', function(request, response) {
   response.sendFile(path.join(__dirname, 'index.html'));
 });
 // Starts the server.
-server.listen(5000, function() {
-  console.log('Starting server on port 5000');
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+server.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", port " + server_port )
 });
 
 let games = {};
